20b006d75cf0bc82a8ab5bb5f32876c876670345
王嘉巍 <wangjiawei@wangjiaweideMacBook.local>
2018-09-14 00:00:31 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,21 +2,33 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .List
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    background: red
 </style>
