3e73733b540be3f49c4c47d02e7edd8c4c430bb1
王嘉巍 <wangjiawei@wangjiaweideMacBook.local>
2018-09-25 21:14:12 +0800

    use vuex

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -13,6 +13,7 @@
           class="search-item border-bottom"
           v-for="item of list"
           :key="item.id"
+          @click="handleCityClick(item.name)"
         >
           {{item.name}}
         </li>
@@ -26,6 +27,7 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapMutations } from 'vuex'
 export default {
   name: 'CitySearch',
   props: {
@@ -64,6 +66,13 @@ export default {
         this.list = result
       }, 100)
     }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+    ...mapMutations(['changeCity'])
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.search)
