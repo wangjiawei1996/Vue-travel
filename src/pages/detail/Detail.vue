fda757cb9c94d33529e8d983d4515efb11bfc123
王嘉巍 <906789088@qq.com>
2018-10-09 19:52:27 +0800

    fix: header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <detail-banner></detail-banner >
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
-  },
-  mounted () {
-
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
-<style  scoped>
-.a {
-  position: absolute;
-}
+<style lang="stylus" scoped>
+  .content
+    height: 50rem
 </style>
